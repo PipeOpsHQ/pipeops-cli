f45768111835e7f1d7aa22879ec7002739eb2c20
Alex Idowu <alexidowu25@gmail.com>
2024-12-26 06:27:11 +0100

    introduce installing agents and components into CLI

---
+++ cmd/agent/agent.go
@@ -0,0 +1,17 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+type agentModel struct {
+	rootCmd *cobra.Command
+}
+
+func NewAgent(rootCmd *cobra.Command) *agentModel {
+	return &agentModel{
+		rootCmd: rootCmd,
+	}
+}
+
+func (a *agentModel) Register() {
+	a.install()
+}
